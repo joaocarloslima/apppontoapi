6b291d7a89ff78acfbf50166889038ea6bc297dc
joaocarloslima <joaocarloslima@me.com>
2024-12-03 15:36:27 -0300

    change to mysql

---
+++ src/main/resources/static/tailwind-config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#yellow-400',
+                'background': '#gray-800',
+            },
+            fontFamily: {
+                'sans': ['Arial', 'sans-serif'],
+            },
+        }
+    }
+}
